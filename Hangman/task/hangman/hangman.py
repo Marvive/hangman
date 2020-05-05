32ee313e52eeffae2df5e61fdb4bfccb5ae22d96
Michael Marvive <marvive42@gmail.com>
2020-05-05 15:39:44 -0700

    Some updates for next task

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,19 +1,30 @@
 import random
 
 choices = ['python', 'java', 'kotlin', 'javascript']
-correct = random.choice(choices)
-correct_length = len(correct)
-# removed = correct[3:correct_length]
+answer = random.choice(choices)
+answer_length = len(answer)
+# removed = answer[3:answer_length]
 # rem_num = len(removed)
-# modified_string = correct.rstrip(removed)
-
+# modified_string = answer.rstrip(removed)
+hidden_word = answer_length * "-"
+tries = 8
+unsolved = True
 
 print("H A N G M A N")
 # print(f"Guess the word {modified_string}" + "-" * rem_num + ": > ", end='')
-print(f"Guess the word " + "-" * correct_length + ": > ", end='')
+print(f"Guess the word " + hidden_word + ": > ", end='')
 word = input()
 
-if word == correct:
+while tries > -1 and unsolved:
+    attempt_word = input()
+    if attempt_word in answer:
+        print("Good Choice!")
+    else:
+        print("Bad Choice")
+        tries -= 1
+
+if word == answer:
     print("You survived!")
-else:
+else:
     print("You are hanged!")
+
