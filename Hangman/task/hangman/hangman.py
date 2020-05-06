477738b594af71496e80c49c33265a85946e6763
Michael Marvive <marvive42@gmail.com>
2020-05-05 18:07:02 -0700

    Some updates for next task

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -10,12 +10,14 @@ tries = 8
 unsolved = True
 
 print("H A N G M A N")
-print(f"Guess the word " + hidden_word + ": > ", end='')
+print()
+# print(f"Guess the word " + hidden_word + ": > ", end='')
 
 while tries > -1 and unsolved:
-    attempt_word = input()
+    print(hidden_word)
+    attempt_word = input("Input a letter : ")
     if attempt_word in answer:
-        print("Good Choice!")
+        # print("Good Choice!")
         counter = 0
         for chars in answer:
             if chars == attempt_word:
@@ -23,16 +25,21 @@ while tries > -1 and unsolved:
             counter += 1
         hidden_word = ''.join(hidden_word_array)
     else:
-        print("Bad Choice")
-        if tries != 1:
-            print(f"You have {tries} tries left.")
-        else:
-            print(f"You have 1 try left.")
+        print("No such letter in the word")
+        # if tries != 1:
+        #     print(f"You have {tries} tries left.")
+        # else:
+        #     print(f"You have 1 try left.")
         tries -= 1
-    print(hidden_word)
+    # print(hidden_word)
+    print()
+    if "-" not in hidden_word:
+        unsolved = False
+        break
 
-if unsolved:
-    print("You died!")
-else:
-    print("You live!")
+print("Thanks for playing!")
+# if unsolved:
+#     print("You died!")
+# else:
+#     print("You live!")
 
